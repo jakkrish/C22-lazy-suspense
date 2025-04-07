8e41e77bb548b464661329ef99cfb49806d93c32
jakkrish <jak.krish@gmail.com>
2025-04-07 21:06:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { lazy, Suspense } from 'react';
+
+import Loading from './Loading.tsx';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Loading />
+    </>
+  );
+}
+
+export default App;
